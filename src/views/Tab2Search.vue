0d95601da4a1c3a75ccbcaffa4b126ad35644e55
SarraBenLtaief <sarra_ben@hotmail.it>
2022-02-20 18:25:37 +0100

    added simple modal

--- src/views/Tab2Search.vue
+++ src/views/Tab2Search.vue
@@ -45,9 +45,13 @@
                 {{car.distance}}
               </ion-col>
             </ion-row>
-          
-            <ion-button expand="block" color="primary">View Car Park</ion-button>
+
             
+            <ion-button @click="openModal" expand="block" color="primary">View Car Park</ion-button>
+
+            
+
+
           </ion-card-content>
         </ion-card>
       </ion-list>
@@ -62,28 +66,65 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from 'vue';
+import { defineComponent, computed} from 'vue';
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar,
-IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonImg} from '@ionic/vue';
+IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonImg, modalController} from '@ionic/vue';
 import carsData from "../parkingSpaces.json";
 
+import SimpleModal from '@/components/SimpleModal.vue'
 
 export default defineComponent({
   name: 'Tab2Search',
   components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonSearchbar,
-                IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonImg },
+                IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, 
+                IonImg},
   setup() {
   const image = computed(() => require('../../resources/parkinglot.jpeg'))
 
-    return {
-      image,
-    }
+    return { image, }
   },
   data() {
     return {
       cars: carsData,
     };
   },
+  dataModal() {
+    return {
+      isOpen: false,
+      modal: null,
+    }
+  },
+  methods: {
+    /** called when the modal is closed */
+    modalCloseHandler(_value: { success: any; noteInfo: any; }) {
+      console.log("modal-closed", _value);
+      if (_value.success) {
+        // only on success
+        alert(JSON.stringify(_value.noteInfo, null, 2));
+      }
+    },
+    /**
+     * when the user clicks button, we open the modal
+     */
+    async openModal() {
+      let modal = await modalController.create({
+        component: SimpleModal,
+        componentProps: {
+          propsData: {
+            timeStamp: new Date()
+          }
+        }
+      });
+      // show the modal
+      await modal.present();
+      // wait to see if i get a response
+      let modalResponse = await modal.onDidDismiss();
+      // when dismissed by clicking outside of modal,
+      // data is undefined so we do not handle it
+      modalResponse.data && this.modalCloseHandler({...modalResponse.data})
+    }
+  }
+
 
 });
 </script>
